b099b082dc66bdd6f96be1d5850aae463c12c88f
Linas Beresna <libe@dneg.com>
2020-02-04 16:12:24 +0000

    cleanup

--- src/cmake/modules/Findbraw.cmake
+++ src/cmake/modules/Findbraw.cmake
@@ -1,7 +1,6 @@
 # - Find braw
 # Find the LibRaw library <http://www.libraw.org>
 # This module defines
-#  BRAW_VERSION_STRING, the version string of braw
 #  BRAW_INCLUDE_DIR, where to find the BlackmagicRAWAPI.h
 #  BRAW_LIBRARIES, the libraries needed to use braw
 #  BRAW_DEFINITIONS, the definitions needed to use braw
@@ -19,7 +18,7 @@ IF(PKG_CONFIG_FOUND AND NOT BRAW_PATH)
 
 ENDIF()
 
-FIND_PATH(BRAW_INCLUDE_DIR BlackmagicRawAPI.h LinuxCOM.h
+FIND_PATH(BRAW_INCLUDE_DIR BlackmagicRawAPI.h   # LinuxCOM.h must also be present
 	HINTS
 	${BRAW_INCLUDEDIR_HINT}
 	${BRAW_PATH}
@@ -29,7 +28,9 @@ FIND_PATH(BRAW_INCLUDE_DIR BlackmagicRawAPI.h LinuxCOM.h
 	PATH_SUFFIXES libraw
 	)
 
-FIND_LIBRARY(BRAW_LIBRARIES NAMES braw	libBlackmagicRawAPI BlackmagicRawAPI
+# BlackmagicRAW also requires libc++ and libc++abi
+
+FIND_LIBRARY(BRAW_LIBRARIES NAMES braw libBlackmagicRawAPI BlackmagicRawAPI
 	HINTS
 	${BRAW_LIBDIR_HINT}
 	${BRAW_PATH}
@@ -37,30 +38,9 @@ FIND_LIBRARY(BRAW_LIBRARIES NAMES braw	libBlackmagicRawAPI BlackmagicRawAPI
 	${PC_BRAW_LIBRARY_DIRS}
 	)
 
-message (STATUS "BRAW_LIBRARIES: ${BRAW_LIBRARIES}" )
-message (STATUS "BRAW_INCLUDE: ${BRAW_INCLUDE_DIR}" )
+# BlackmagicRAW doesn't seem to have information about versioning...
+# Will have to continue to check their website to see if there are any versioning going on
 
-# IF(LibRaw_INCLUDE_DIR)
-# FILE(READ ${LibRaw_INCLUDE_DIR}/libraw/libraw_version.h _libraw_version_content)
-#
-# STRING(REGEX MATCH "#define LIBRAW_MAJOR_VERSION[ \t]*([0-9]*)\n" _version_major_match ${_libraw_version_content})
-# SET(_libraw_version_major "${CMAKE_MATCH_1}")
-#
-# STRING(REGEX MATCH "#define LIBRAW_MINOR_VERSION[ \t]*([0-9]*)\n" _version_minor_match ${_libraw_version_content})
-# SET(_libraw_version_minor "${CMAKE_MATCH_1}")
-#
-# STRING(REGEX MATCH "#define LIBRAW_PATCH_VERSION[ \t]*([0-9]*)\n" _version_patch_match ${_libraw_version_content})
-# SET(_libraw_version_patch "${CMAKE_MATCH_1}")
-#
-# IF(_version_major_match AND _version_minor_match AND _version_patch_match)
-# SET(LibRaw_VERSION_STRING "${_libraw_version_major}.${_libraw_version_minor}.${_libraw_version_patch}")
-# ELSE()
-# IF(NOT LibRaw_FIND_QUIETLY)
-# MESSAGE(STATUS "Failed to get version information from ${LibRaw_INCLUDE_DIR}/libraw/libraw_version.h")
-# ENDIF()
-# ENDIF()
-# ENDIF()
-#
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(braw
 	REQUIRED_VARS BRAW_LIBRARIES BRAW_INCLUDE_DIR
